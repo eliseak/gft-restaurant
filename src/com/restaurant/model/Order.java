6f9c108c24e17277a5e9fcc58dd2d449c267d33b
Elise AK <eliseakemi@gmail.com>
2018-07-20 06:23:09 -0300

    Add base properties and methods for Order

--- src/com/restaurant/model/Order.java
+++ src/com/restaurant/model/Order.java
@@ -1,5 +1,35 @@
 package com.restaurant.model;
 
-public class Order {
+import java.util.Arrays;
 
+public class Order {
+	private TimeOfDay time;
+	private int[] dishes;
+	
+	public TimeOfDay getTime() {
+		return time;
+	}
+	public void setTime(TimeOfDay time) {
+		this.time = time;
+	}
+	public int[] getDishes() {
+		return dishes;
+	}
+	public void setDishes(int[] dishes) {
+		this.dishes = dishes;
+	}
+	
+	@Override
+	public String toString() {
+		return "Order [time=" + time + ", dishes=" + Arrays.toString(dishes) + "]";
+	}
+	
+	public String printOrder() {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(this.toString());
+		
+		return sb.toString();
+	}
+	
 }
